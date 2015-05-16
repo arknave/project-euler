07e37a35e41fcf25c1134579ac5e7535e00724cd
arknave <arnavsastry@gmail.com>
2015-05-15 22:02:28 -0500

    very slow p10

---
+++ scala/pe10.scala
@@ -0,0 +1,7 @@
+object ProjectEuler10 {
+  lazy val primes: Stream[Long] = 2 #:: Stream.from(3, 2).filter(x => primes.takeWhile(p => p * p <= x).forall(p => x % p != 0)).map(_.toLong)
+
+  def main(args: Array[String]) = {
+    println(primes.takeWhile(_ < 2000000).sum)
+  }
+}
